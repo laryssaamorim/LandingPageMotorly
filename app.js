8b32efbaad17d09253b2e5e695bb608adb24ee18
laryssaamorim <amorimlaryssajf@gmail.com>
2021-01-16 17:15:17 -0300

    Adicao de novos arquivos

---
+++ app.js
@@ -0,0 +1,39 @@
+$('.menu-nav a[href^="#"]').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('.menu').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+$('.logo').click(function(e){
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500)
+});
+
+$('section').each(function(){
+	var height = $(this).height(),
+			offsetTop = $(this).offset().top,
+			menuHeight = $('.menu').innerHeight(),
+			id = $(this).attr('id'),
+			$itemMenu = $('a[href="#' + id + '"]');
+	
+	$(window).scroll(function(){
+		var scrollTop = $(window).scrollTop();
+		if(offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
+			$itemMenu.addClass('active');
+		} else {
+			$itemMenu.removeClass('active');
+		}
+	});
+});
+
+
+$('.mobile_btn').click(function(){
+	$(this).toggleClass('active');
+	$('.mobile-menu').toggleClass('active');
+});
